e4cfa18b37b202258ffb98e740c48ba76b1d66ef
Vivek Patare <vivek.patare@students.iiit.ac.in>
2018-11-01 23:45:51 +0530

    with pickle (shipment, delivery remaining)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pickle
 
-products = []
-customers = []
 product_itr = 0
 customer_itr = 0
 
@@ -34,11 +33,70 @@ class Customer(Cart):
     def viewProducts(self):
         print("---------------------------------------------------------")
         print("id   name    price   quantity    group   subgroup")
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
+        product_list.close()
         for product in products:
             print(product.product_id, product.name, product.price, product.count, product.group, product.subgroup)
         print("---------------------------------------------------------")
-    #def addToCard(self):
 
+    def addToCard(self, products, customers, product_index, quantity_to_buy):
+        self.product_ids.append(products[product_index].product_id)
+        self.numberofproducts += 1
+        self.product_names.append(products[product_index].name)
+        self.prices.append(products[product_index].price)
+        self.quantities.append(quantity_to_buy)
+        self.total = str(int(self.total) + int(products[product_index].price) * int(quantity_to_buy))
+        products[product_index].count = str(int(products[product_index].count) - int(quantity_to_buy))
+        customer_list = open("customers.pickle", "wb")
+        product_list = open("products.pickle", "wb")
+        pickle.dump(customers, customer_list)
+        pickle.dump(products, product_list)
+        customer_list.close()
+        product_list.close()
+        print("added to cart successfully")
+
+    def deleteFromCart(self, products, customers, dummy_admin, product_id_to_delete):
+        self.numberofproducts -= 1
+        product_index_to_change = dummy_admin.existsProduct(product_id_to_delete)
+        cart_index_to_delete = self.product_ids.index(product_id_to_delete)
+        self.total = str(int(self.total) - int(self.prices[cart_index_to_delete]) * int(
+            self.quantities[cart_index_to_delete]))
+        del self.product_ids[cart_index_to_delete]
+        del self.prices[cart_index_to_delete]
+        del self.product_names[cart_index_to_delete]
+        products[product_index_to_change].count = str(
+            int(products[product_index_to_change].count) + int(self.quantities[cart_index_to_delete]))
+        del self.quantities[cart_index_to_delete]
+        customer_list = open("customers.pickle", "wb")
+        product_list = open("products.pickle", "wb")
+        pickle.dump(customers, customer_list)
+        pickle.dump(products, product_list)
+        customer_list.close()
+        product_list.close()
+        print("product deleted from cart successfully")
+
+    def viewCart(self):
+        print("Total products in cart = ", self.numberofproducts)
+        print("---------------------------------------------------------")
+        print("id  name     quantity    price")
+        for i in range(len(self.product_names)):
+            print(self.product_ids[i], self.product_names[i], self.quantities[i], self.prices[i])
+        print("---------------------------------------------------------")
+        print("Total Amount = ", self.total)
+
+    def buyProducts(self, customers):
+        self.viewCart()
+
+        confirm = input("Confirm Buy (y/n):")
+        if confirm.lower() == "y":
+            print("amount of ", self.total, "is paid")
+            Cart.__init__(self)
+            customer_list = open("customers.pickle", "wb")
+            pickle.dump(customers, customer_list)
+            customer_list.close()
+            print("Transaction done")
+            return
 
 
 
@@ -52,16 +110,22 @@ class Admin():
     def viewProducts(self):
         print("----------------------------------------------------------")
         print("id   name    price   quantity    group   subgroup")
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
         for product in products:
             print(product.product_id, product.name, product.price, product.count, product.group, product.subgroup)
         print("----------------------------------------------------------")
+        product_list.close()
 
 
     def existsProduct(self, id):
         found = 0
         itr = 0
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
+        product_list.close()
         for product in products:
-            if product.product_id == id:
+            if str(product.product_id) == str(id):
                 found = 1
                 break
             itr += 1
@@ -70,21 +134,42 @@ class Admin():
         else:
             return -1
 
+
     def addProduct(self, product):
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
+        product_list.close()
         products.append(product)
-        print("product added successfully")
+        product_list = open("products.pickle", "wb")
+        pickle.dump(products, product_list)
+        product_list.close()
+        # print("product added successfully")
 
     def deleteProduct(self, id):
         index = self.existsProduct(id)
         if index == -1:
             print("product not found")
             return
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
         del products[index]
+        product_list.close()
+        product_list = open("products.pickle", "wb")
+        pickle.dump(products, product_list)
+        product_list.close()
         print("product deleted successfully")
 
     def modifyProduct(self, index, product):
+        product_list = open("products.pickle", "rb")
+        products = pickle.load(product_list)
         products[index] = product
+        product_list.close()
+        product_list = open("products.pickle", "wb")
+        pickle.dump(products, product_list)
+        product_list.close()
         print("Product modified successfully")
+
+
 
 
 
@@ -142,8 +227,11 @@ def admin_functions():
 def exitsCustomer(id):
     found = 0
     itr = 0
+    customer_list = open("customers.pickle", "rb")
+    customers = pickle.load(customer_list)
+    customer_list.close()
     for customer in customers:
-        if customer.customer_id == id:
+        if str(customer.customer_id) == str(id):
             found = 1
             break
         itr += 1
@@ -167,6 +255,8 @@ def guest_functions():
         if choice == "1":
             store_admin = Admin(1, "Vivek")
             store_admin.viewProducts()
+
+            customer_list = open("customers.pickle", "rb")
             continue
 
         if choice == "2":
@@ -175,8 +265,16 @@ def guest_functions():
             if len(item) != 3:
                 print("provide correct details")
                 continue
+            customer_list = open("customers.pickle", "rb")
+            customers = pickle.load(customer_list)
+            customer_list.close()
             customers.append(Customer(customer_itr, item[0], item[1], item[2]))
-            print("Registered Successfully...login with user id")
+            customer_list = open("customers.pickle", "wb")
+            customer_itr += 1
+            pickle.dump(customers, customer_list)
+            customer_list.close()
+
+            print("Registered Successfully with customer_id = ", customer_itr - 1, "...login with user id ")
             return
         if choice == "3":
             print("switching user...")
@@ -190,6 +288,9 @@ def guest_functions():
 def customerfunctions(index):
     global customer_itr
     global product_itr
+    customer_list = open("customers.pickle", "rb")
+    customers = pickle.load(customer_list)
+    print("Welcome", customers[index].name)
     print("1. View Products")
     print("2. Add to cart")
     print("3. Delete from cart")
@@ -197,9 +298,15 @@ def customerfunctions(index):
     print("5. Buy Products")
     print("6. change user type")
 
+    customer_list = open("customers.pickle", "rb")
+    customers = pickle.load(customer_list)
+    customer_list.close()
     customer = customers[index]
     dummy_admin = Admin(1, "Vivek")
 
+    product_list = open("products.pickle", "rb")
+    products = pickle.load(product_list)
+    product_list.close()
     while True:
         choice = input("choice?:")
         if choice == "1":
@@ -215,57 +322,25 @@ def customerfunctions(index):
             if int(quantity_to_buy) > int(products[product_index].count):
                 print("product not available in required quantity")
                 continue
-            customer.product_ids.append(products[product_index].product_id)
-            customer.numberofproducts += 1
-            customer.product_names.append(products[product_index].name)
-            customer.prices.append(products[product_index].price)
-            customer.quantities.append(quantity_to_buy)
-            customer.total = str(int(customer.total) +  int(products[product_index].price) * int(quantity_to_buy))
-            products[product_index].count = str(int(products[product_index].count) -  int(quantity_to_buy))
-            print("added to cart successfully")
+
+            customer.addToCard(products, customers, product_index, quantity_to_buy)
             continue
 
         if choice == "3":
-            product_id_to_delete = input("Enter product id:")
+            product_id_to_delete = input("Enter product id to delete:")
             if not product_id_to_delete in customer.product_ids:
                 print("product with given id doesnt exist in cart")
                 continue
-            customer.numberofproducts -= 1
-            product_index_to_change = dummy_admin.existsProduct(product_id_to_delete)
-            cart_index_to_delete = customer.product_ids.index(product_id_to_delete)
-            customer.total = str(int(customer.total) - int(customer.prices[cart_index_to_delete]) * int(customer.quantities[cart_index_to_delete]))
-            del customer.product_ids[cart_index_to_delete]
-            del customer.prices[cart_index_to_delete]
-            del customer.product_names[cart_index_to_delete]
-            products[product_index_to_change].count = str(int(products[product_index_to_change].count) + int(customer.quantities[cart_index_to_delete]))
-            del customer.quantities[cart_index_to_delete]
-            print("product deleted from cart successfully")
+
+            customer.deleteFromCart(products, customers, dummy_admin, product_id_to_delete)
             continue
 
         if choice == "4":
-            print("Total products in cart = ", customer.numberofproducts)
-            print("---------------------------------------------------------")
-            print("id  name     quantity    price")
-            for i in range(len(customer.product_names)):
-                print(customer.product_ids[i], customer.product_names[i], customer.quantities[i], customer.prices[i])
-            print("---------------------------------------------------------")
-            print("Total Amount = ", customer.total)
+            customer.viewCart()
             continue
 
         if choice == "5":
-            print("Total products in cart = ", customer.numberofproducts)
-            print("---------------------------------------------------------")
-            print("id  name     quantity    price")
-            for i in range(len(customer.product_names)):
-                print(customer.product_ids[i], customer.product_names[i], customer.quantities[i], customer.prices[i])
-            print("---------------------------------------------------------")
-            print("Total Amount = ", customer.total)
-            confirm = input("Confirm Buy (y/n):")
-            if confirm.lower() == "y":
-                print("amount of ", customer.total, "is paid")
-                print("Transaction done")
-                print("logging out.....")
-                return
+            customer.buyProducts(customers)
             continue
 
         if choice == "6":
@@ -290,21 +365,34 @@ def customerfunctions(index):
 def main():
     global product_itr
     global customer_itr
-    with open("products.txt", 'r') as fp:
-        for line in fp:
-            item = line.split()
-            products.append(Product(item[0], item[1], item[2], item[3], item[4], item[5]))
+    # products = []
+    # customers = []
+    # product_list = open("products.pickle", "rb")
+    # customer_list = open("customers.pickle", "wb")
+    # with open("products.txt", 'r') as fp:
+    #     for line in fp:
+    #         item = line.split()
+    #         products.append(Product(item[0], item[1], item[2], item[3], item[4], item[5]))
+    #     pickle.dump(products, product_list)
+    #     product_list.close()
+    #     product_itr = len(products) + 1
+    #
+    #
+    # with open("customers.txt", 'r') as fp:
+    #     for line in fp:
+    #         item = line.split()
+    #         customers.append(Customer(item[0], item[1], item[2], item[3]))
+    #     pickle.dump(customers, customer_list)
+    #     customer_list.close()
+    #     customer_itr = len(customers) + 1
 
-        product_itr = len(products) + 1
-
-
-
-    with open("customers.txt", 'r') as fp:
-        for line in fp:
-            item = line.split()
-            customers.append(Customer(item[0], item[1], item[2], item[3]))
-        customer_itr = len(customers) + 1
-
+    product_list = open("products.pickle", "rb")
+    customer_list = open("customers.pickle", "rb")
+    customers = pickle.load(customer_list)
+    product_itr = len(pickle.load(product_list)) + 1
+    customer_itr = len(customers) + 1
+    product_list.close()
+    customer_list.close()
     while True:
         user_type  = input("guest, customer, admin?:")
         if user_type.lower() == "admin":
